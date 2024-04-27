0e24318cd5ea8d2da43fe61da099f938af2ef7a9
mt64iii <168265160+mt64iii@users.noreply.github.com>
2024-04-27 10:11:52 +0200

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -9,16 +9,16 @@ async def must_join_channel(bot: Client, msg: Message):
         return
     try:
         try:
-            await bot.get_chat_member("syrea_surs", msg.from_user.id)
+            await bot.get_chat_member("I_4_R", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/syrea_surs".isalpha():
-                link = "https://t.me/syrea_surs"
+            if "https://t.me/I_4_R".isalpha():
+                link = "https://t.me/I_4_R"
             else:
                 chat_info = await bot.get_chat("syrea_surs")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @syrea_surs .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @I_4_R .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("اضغط للأشتراك .", url=link)]
@@ -28,4 +28,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @syrea_surs !")
+        print(f"I'm not admin in the MUST_JOIN chat @I_4_R !")
