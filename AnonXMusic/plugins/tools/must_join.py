8f7af2990721470e6e985c933ee8524ca559ff35
mt64iii <168265160+mt64iii@users.noreply.github.com>
2024-05-06 18:28:00 +0200

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,20 +5,20 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/077880":  # Not compulsory
+    if not "https://t.me/O77880":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("077880", msg.from_user.id)
+            await bot.get_chat_member("O77880", msg.from_user.id)
         except UserNotParticipant:
             if "https://t.me/077880".isalpha():
-                link = "https://t.me/077880"
+                link = "https://t.me/O77880"
             else:
-                chat_info = await bot.get_chat("077880")
+                chat_info = await bot.get_chat("O77880")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @077880 .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @O77880 .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("اضغط للأشتراك .", url=link)]
@@ -28,4 +28,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @077880 !")
+        print(f"I'm not admin in the MUST_JOIN chat @O77880 !")
