b5f4a5abb2bc3d6aab4d8e9feed8e6aa7b9ab5ef
mt64iii <168265160+mt64iii@users.noreply.github.com>
2024-05-06 18:27:15 +0200

    Update bbnnq.py

--- AnonXMusic/plugins/tools/bbnnq.py
+++ AnonXMusic/plugins/tools/bbnnq.py
@@ -54,7 +54,7 @@ async def ahmad(client: Client, message: Message):
             [
                 InlineKeyboardButton("المطور🤖", user_id=1480987361),
         
-                InlineKeyboardButton("متجر ابن بغداد", url="https://t.me/077880"),
+                InlineKeyboardButton("اشعار حب ", url="https://t.me/O77880"),
             ],
             ]
         ),
